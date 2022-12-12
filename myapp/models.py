383d6a10ea810e7840613da34cf61c608f4fcd72
Kevin Schafer <kevinschafersoftware@gmail.com>
2022-12-12 20:55:37 -0300

    Adding a new file

--- myapp/models.py
+++ myapp/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -15,4 +17,4 @@ class Task(models.Model):
     done = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title + ' - ' + self.project.name
+        return self.title + " - " + self.project.name
